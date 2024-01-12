75ea69bf88f16e057bfdd4ea0ca1471c4f1f145d
John Alexander Jimenez <john.jimenez@puntoscolombia.com>
2024-01-11 21:21:52 -0500

    Future provider

---
+++ lib/presentation/providers/future_providers.dart
@@ -0,0 +1,15 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:riverpod_app/config/config.dart';
+
+part 'future_providers.g.dart';
+
+@Riverpod(keepAlive: true)
+Future<String> pokemonName(PokemonNameRef ref) async {
+  final pokemonName = await PokemonInformation.getPokemonName(1);
+
+  ref.onDispose(() {
+    //solo funcionaria si no se mantiene vivo con el keepALive
+    print('Estamos a punto de eliminar este provider');
+  });
+  return pokemonName;
+}
---
+++ lib/presentation/providers/future_providers.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'future_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$pokemonNameHash() => r'16cddf1dce9ce552d70765b31a1c43fc560559b1';
+
+/// See also [pokemonName].
+@ProviderFor(pokemonName)
+final pokemonNameProvider = FutureProvider<String>.internal(
+  pokemonName,
+  name: r'pokemonNameProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$pokemonNameHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef PokemonNameRef = FutureProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member

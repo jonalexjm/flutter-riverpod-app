d5f5161b11fbb275f189bf2bc503520ab8fa4156
John Alexander Jimenez <john.jimenez@puntoscolombia.com>
2024-01-11 22:33:05 -0500

    stream provider

--- lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
+++ lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/stream_provider.dart';
 
 class StreamProviderScreen extends StatelessWidget {
   const StreamProviderScreen({super.key});
@@ -10,6 +11,36 @@ class StreamProviderScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Stream Provider'),
       ),
+      body: const StreamView(),
     );
+  }
+}
+
+class StreamView extends ConsumerWidget {
+  const StreamView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userInChatAsync = ref.watch(usersInChatProvider);
+
+    if (userInChatAsync.isLoading) {
+      return const CircularProgressIndicator();
+    }
+    if (userInChatAsync.hasError) {
+      return Center(
+        child: Text('${userInChatAsync.error}'),
+      );
+    }
+
+    final users = userInChatAsync.value;
+
+    return ListView.builder(
+        itemCount: users!.length,
+        itemBuilder: (BuildContext context, int index) {
+          final user = users[index];
+          return ListTile(
+            title: Text(user),
+          );
+        });
   }
 }
